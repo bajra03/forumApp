cc8943786c5962c1827aa21e47b75c20b74cf330
Bajra <putu.bajra@gmail.com>
2019-01-15 14:33:06 +0800

    CRUD for Question along with Question Resource Transformation

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,6 +7,23 @@ use App\User;
 
 class Question extends Model
 {
+    // This method old school, imagine if you have so many field and you put that one by one. So we are going to use protected $guarded
+    // protected $fillable = [
+    //     'title',
+    //     'slug',
+    //     'body',
+    //     'category_id',
+    //     'user_id'
+    // ];
+
+    // New method using protected $guarded
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return ('slug');
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -20,5 +37,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
