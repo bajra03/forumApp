e45b986fadc8d7f6adb578e367172e0a9e321120
Bajra <putu.bajra@gmail.com>
2019-01-17 14:12:55 +0800

    Like and dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ Route::apiResource('/category', 'CategoryController');
 
 // Route for Replies
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+// Route for Like
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
